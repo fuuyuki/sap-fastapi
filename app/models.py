7e546a417adb9876be629f85e1f788be8ef3dd93
Muhammad Fikra Adzaky <fikraadzaky123@gmail.com>
2026-05-23 15:43:01 +0700

    bringback notif and change name into patient_id

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from sqlalchemy import (
     TIME,
     TIMESTAMP,
     UUID,
+    CheckConstraint,
     Column,
     DateTime,
     ForeignKey,
@@ -35,27 +36,34 @@ users = Table(
     Column("name", String(100), nullable=False),
     Column("email", String(100), unique=True, nullable=False),
     Column("password", String(255), nullable=False),
-    Column("role", String(50), default="patient", nullable=False),  # e.g. patient/admin
+    Column("role", String(50), nullable=False),
+    Column("caretaker_id", UUID(as_uuid=True), ForeignKey("users.id"), nullable=True),
+    # Constraints
+    CheckConstraint(
+        "(role = 'patient' AND caretaker_id IS NOT NULL) "
+        "OR (role = 'caretaker' AND caretaker_id IS NULL)",
+        name="patient_caretaker_check",
+    ),
+    CheckConstraint("role IN ('caretaker','patient')", name="role_check"),
 )
 
 # 2. Devices
 devices = Table(
     "devices",
     metadata,
-    Column("chip_id", String(100), primary_key=True),  # ESP32 hardware ID
-    Column("user_id", UUID(as_uuid=True), ForeignKey("users.id"), nullable=False),
+    Column("chip_id", String(100), primary_key=True),
     Column("name", String(100), nullable=False),
-    Column("status", String(50), default="offline"),
+    Column("status", String(50), server_default=text("'offline'")),
     Column(
         "last_seen",
         TIMESTAMP(timezone=True),
-        server_default=func.now(),
+        server_default=text("now()"),
         nullable=False,
     ),
-    Column(
-        "api_key", String(255), nullable=False, unique=True, default=generate_api_key
-    ),  # for ESP32 authentication
+    Column("api_key", String(255), nullable=False),
+    Column("patient_id", UUID(as_uuid=True), ForeignKey("users.id"), nullable=False),
 )
+
 # 3. Schedules
 schedules = Table(
     "schedules",
@@ -66,7 +74,7 @@ schedules = Table(
         primary_key=True,
         server_default=text("uuid_generate_v4()"),
     ),
-    Column("user_id", UUID(as_uuid=True), ForeignKey("users.id"), nullable=False),
+    Column("patient_id", UUID(as_uuid=True), ForeignKey("users.id"), nullable=False),
     Column("device_id", String(100), ForeignKey("devices.chip_id"), nullable=False),
     Column("pillname", String(100), nullable=False),
     Column("dose_time", TIME(timezone=False), nullable=False),
@@ -83,11 +91,32 @@ medlogs = Table(
         primary_key=True,
         server_default=text("uuid_generate_v4()"),
     ),
-    Column("user_id", UUID(as_uuid=True), ForeignKey("users.id"), nullable=False),
+    Column("patient_id", UUID(as_uuid=True), ForeignKey("users.id"), nullable=False),
     Column("device_id", String(100), ForeignKey("devices.chip_id"), nullable=False),
     Column("pillname", String(100), nullable=False),
     Column("scheduled_time", TIMESTAMP(timezone=False), nullable=False),
     Column("status", String(50), nullable=False),  # "taken" or "missed"
+)
+
+# 5. Notifications
+notifications = Table(
+    "notifications",
+    metadata,
+    Column(
+        "id",
+        UUID(as_uuid=True),
+        primary_key=True,
+        server_default=text("uuid_generate_v4()"),
+    ),
+    Column("device_id", String(100), ForeignKey("devices.chip_id"), nullable=False),
+    Column("user_id", UUID(as_uuid=True), ForeignKey("users.id"), nullable=False),
+    Column("message", String(255), nullable=False),
+    Column(
+        "created_at",
+        TIMESTAMP(timezone=False),
+        server_default=func.now(),
+        nullable=False,
+    ),
 )
 
 # 6. Device Tokens (for push notifications)
